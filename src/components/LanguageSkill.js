2b53a263c6ca75ec077be95a7dbad7d16455a748
ayaaaaaka <ayaka_kobayashi@voyagegroup.com>
2024-07-20 23:17:24 +0900

    一旦これでいいわ

--- src/components/LanguageSkill.js
+++ src/components/LanguageSkill.js
@@ -3,9 +3,17 @@ import React from 'react';
 const LanguageSkill = ({ language, level }) => {
   const numericLevel = parseInt(level, 10);
   const dots = Array(5).fill(0).map((_, index) => (
+    //TODO: きれいにしたい
     <span
       key={index}
-      className={`border ${index < numericLevel ? 'bg-black border-black' : 'bg-white border-black'}`}
+      style={{
+        width: '16px',
+        height: '16px',
+        borderRadius: '50%',
+        margin: '0 4px',
+        border: '1px solid black',
+        backgroundColor: index < level ? 'black' : 'white'
+      }}
     ></span>
   ));
 
