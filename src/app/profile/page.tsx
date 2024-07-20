d81320d3156680e008e8abbbf04c9b4b77cc5dc0
ayaaaaaka <ayaka_kobayashi@voyagegroup.com>
2024-07-20 23:42:43 +0900

    右側

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -39,27 +39,33 @@ const ProfilePage = () => {
         </div>
         <div className="work-details">
           <WorkExperience 
-            year="2017" 
+            year="2016" 
             company="株式会社 Gods" 
-            role="iOSエンジニア" 
+            role="iOSエンジニア アルバイト" 
             details={["受託開発", "某ジムの受付アプリ"]} 
           />
           <WorkExperience 
             year="2017" 
             company="株式会社 CANDLE" 
-            role="Webデザイン" 
+            role="iOSエンジニア アルバイト" 
             details={["自社開発", "女性向けアプリの改修・保守"]} 
           />
           <WorkExperience 
             year="2018" 
+            company="株式会社 VOYAGE GROUP" 
+            role="webエンジニア アルバイト" 
+            details={["自社開発", "ポイントサイトの設計・開発・保守"]} 
+          />
+          <WorkExperience 
+            year="2019" 
             company="株式会社 VOYAGE GROUP 入社" 
-            role="webエンジニア" 
+            role="webエンジニア 正社員" 
             details={["自社開発", "ポイントサイトの設計・開発・保守"]} 
           />
           <WorkExperience 
             year="2020" 
             company="株式会社 CARTA HOLDINGS" 
-            role="フルスタックエンジニア" 
+            role="フルスタックエンジニア 正社員" 
             details={["自社開発", "ポイントサイトの企画・設計・開発・保守", "プロダクトマネージャー"]} 
           />
         </div>
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -2,15 +2,37 @@ import React from 'react';
 
 const WorkExperience = ({ year, company, role, details }) => {
   return (
-    <div className="work-experience">
-      <h3>{year}</h3>
-      <p>{company}</p>
-      <p>{role}</p>
-      <ul>
-        {details.map((detail, index) => (
-          <li key={index}>{detail}</li>
-        ))}
-      </ul>
+    <div className="flex mb-10 work-experience" style={{marginTop: '40px'}}>
+      <div className="flex-shrink-0 w-16 text-right pr-4">
+        <span className="text-xl font-semibold">{year}</span>
+      </div>
+      <span
+        style={{
+          width: '13px',
+          height: '13px',
+          backgroundColor: 'black',
+          borderRadius: '50%',
+          border: '1px solid black',
+          display: 'block',
+          marginLeft: '10px',
+          marginRight: '10px',
+          marginTop: '5px',
+        }}
+      ></span>
+      <div className="flex-grow border-l-2 border-gray-300 pl-4 relative">
+        <div className="mb-2">
+          <span className="block font-semibold">{company}</span>
+        </div>
+        <span className="block">{role}</span>
+        <ul className="list-disc list-inside">
+          {details.map((detail, index) => (
+            <li key={index}>{detail}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="absolute -left-8 top-0 h-full flex items-center justify-center">
+      <span className="w-4 h-4 bg-black rounded-full border border-black"></span>
+    </div>
     </div>
   );
 };
