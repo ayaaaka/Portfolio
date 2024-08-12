692f74cac2d67cc3a653abfdd8478c4b6b0e6fb1
ayaaaaaka <ayaka_kobayashi@voyagegroup.com>
2024-08-12 19:15:54 +0900

    fix style崩れ

--- src/components/ui/HobbyCard.tsx
+++ src/components/ui/HobbyCard.tsx
@@ -8,9 +8,9 @@ interface HobbyCardProps {
 
 export function HobbyCard({ title, icon }: HobbyCardProps) {
     return (
-        <div className="p-4 border rounded-lg" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-            <FontAwesomeIcon icon={icon} width={100} />
-            <p style={{ textAlign: 'center', marginTop: '10px' }}>{title}</p>
+        <div className="p-4 border rounded-xl text-center">
+            <FontAwesomeIcon icon={icon} width={100} className="mx-auto"/>
+            <p className="text-center mt-2.5">{title}</p>
         </div>
     );
 }
